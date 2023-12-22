36a21c187629190ece6de2e40e6870cecc17c1d6
EcoSiege <153530802+EcoSiege@users.noreply.github.com>
2023-12-22 10:53:06 -0600

    added comments to the javascript

--- Develop/script.js
+++ Develop/script.js
@@ -12,35 +12,27 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
+// Arrays for each character type
 const big = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '/', '?', '.', '>', '<', ',', '{', '}', '[', ']', '`', '~', ';', ':', '"', "'", '|'];
 
-
-
-
-
-
-
-// prompt(), alert(), confirm()
 function generatePassword() {
   selection = [];
   var length = prompt("How long do you want your password to be?\nMininum length is 8 characters.\nMaximum length is 128 characters.");
+  // Ends function if there is no imput
   if (!length) {
     return;
+  // Ends function if the input is too long or too short
   } else if (length < 8) {
     alert("Password length must be inbetween 8 and 128.");
     return;
   } else if (length > 128) {
     alert("Password length must be inbetween 8 and 128.");
     return;
-  } else if (length !== number){
-    alert("Please input a number inbetween 8 and 128.");
-    return;
   };
-
+  // Prompts for the user to pick character types
   var lowercase = confirm("Do you want to include lowercase letters?");
   var uppercase = confirm("Do you want to include uppercase letters?");
   var number = confirm("Do you want to include numbers?");
@@ -48,7 +40,7 @@ function generatePassword() {
   var password
 
 
-
+  // Concats selected character types into one array
   if (lowercase) {selection = selection.concat(small)};
   if (uppercase) {selection = selection.concat(big)};
   if (number) {selection = selection.concat(numbers)};
@@ -57,8 +49,7 @@ function generatePassword() {
     alert("Select at least one character type.")
     return
   }
-  console.log(selection.length)
-
+  // Picks random array elements untill users selected lenght is reached
   for (var i = 0; i < length; i++){
     var randomNumber = Math.floor(Math.random() * selection.length );
     console.log(randomNumber, selection[randomNumber]);
@@ -68,6 +59,7 @@ function generatePassword() {
       password = password + selection[randomNumber];
     };
     console.log(password);
+    document.querySelector('#password').textContent = password
   };
-  document.textarea.textcontent = password
+  
 };
